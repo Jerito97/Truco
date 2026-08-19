9531c4b6525ec886a1e0cd3642b400b1bb02dd90
Jerito97 <46766840+Jerito97@users.noreply.github.com>
2026-08-19 19:22:19 -0300

    Add files via upload

--- src/components/HomeDashboard.tsx
+++ src/components/HomeDashboard.tsx
@@ -13,7 +13,7 @@ export function HomeDashboard({
   return (
     <div className="min-h-[calc(100vh-120px)] flex flex-col items-center">
       <div className="flex-[4]" />
-      <img src="/wordmark.webp" alt="Osobuco" className="w-64" />
+      <img src="/wordmark.webp" alt="Osobuco" className="w-80" />
       <div className="flex-[3]" />
       <button
         type="button"
